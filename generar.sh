1bb52412da4e7b709d988c8230510b079a6e4660
Damiàn Grimaldi <grimaldidamian001@gmail.com>
2023-06-08 19:04:05 -0300

    Aca agregamos que elija un nombre al azar para que se pueda guardar los archivos .jpg y a la vez verifica que no se haya creado antes

--- generar.sh
+++ generar.sh
@@ -5,17 +5,27 @@ clear
 read -p $'Ingrese cuantas imagenes quieres descargar\n' cantidad
 
 if [ $cantidad -gt 0 ]; then
-	nombres=$(curl https://raw.githubusercontent.com/fernandezpablo85/name_suggestions/master/assets/dict.csv | grep -E  |  tr ' ' '_' | cut -d ',' -f 1 | head -n $cantidad )
-	clear
-	sleep 4s
-	for nombre in $nombres; do
-		wget -O $nombre.jpg https://source.unsplash.com/random/900%C3%97700/?person &> /dev/null
-		sleep 10s
-		echo La imagen $nombre.jpg se descargo
-	done
+	cont=1
+	$(curl https://raw.githubusercontent.com/fernandezpablo85/name_suggestions/master/assets/dict.csv | tr ' ' '_' | cut -d ',' -f 1) > nombres.txt
+	while [ $cont -le $cantidad ]; do
+		
+		nombre=$( shuf -n 1 nombres.txt )
+		
+		if [ $nombre -ne *.jpg ] ;then
+			
+			cont=$(($cont + 1 | bc))
+			wget -O $nombre.jpg https://source.unsplash.com/random/900%C3%97700/?person &> /dev/null
+			sleep 10s
+			echo La imagen $nombre.jpg se descargo
+			
+		fi
+		
 else
 	echo "Se ingreso un numero menor o igual que 0" && exit 1
 fi
 
-tar -czvf imagenes.tar.gz *.jpg
-sha256sum archivo.tar.gz > sumaVerificadora.txt
+tar -czvf imagenes.tar.gz *.jpg && echo Se creo el archivo imagenes.tar.gz correctamente && rm *.jpg
+
+sha256sum archivo.tar.gz > sumaVerificadora.txt && echo Se creo el archivo de la suma de verificacion
+
+rm nombres.txt
