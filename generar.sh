86a52d6ebfdbe6baaa0b654510279c1a0ac2658d
Damiàn Grimaldi <grimaldidamian001@gmail.com>
2023-06-09 21:48:56 -0300

    Corrección de la linea cut, para que cree el archivo de texto para ser usado en el comando shuf (que toma aleatorio una linea de ahi)

--- generar.sh
+++ generar.sh
@@ -6,14 +6,14 @@ read -p $'Ingrese cuantas imagenes quieres descargar\n' cantidad
 
 if [ $cantidad -gt 0 ]; then
 	cont=1
-	$(curl https://raw.githubusercontent.com/fernandezpablo85/name_suggestions/master/assets/dict.csv | tr ' ' '_' | cut -d ',' -f 1) > nombres.txt
+	curl https://raw.githubusercontent.com/fernandezpablo85/name_suggestions/master/assets/dict.csv | tr ' ' '_' | cut -d ',' -f 1 > nombres.txt
 	while [ $cont -le $cantidad ]; do
 		
 		nombre=$( shuf -n 1 nombres.txt )
 		
 		if [ $nombre -ne *.jpg ] ;then
 			
-			cont=$(($cont + 1 | bc))
+			cont=$(($cont + 1))
 			wget -O $nombre.jpg https://source.unsplash.com/random/900%C3%97700/?person &> /dev/null
 			sleep 10s
 			echo La imagen $nombre.jpg se descargo
@@ -26,6 +26,6 @@ fi
 
 tar -czvf imagenes.tar.gz *.jpg && echo Se creo el archivo imagenes.tar.gz correctamente && rm *.jpg
 
-sha256sum archivo.tar.gz > sumaVerificadora.txt && echo Se creo el archivo de la suma de verificacion
+sha256sum imagenes.tar.gz > sumaVerificadora.txt && echo Se creo el archivo de la suma de verificacion
 
 rm nombres.txt
