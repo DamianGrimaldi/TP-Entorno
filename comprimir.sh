3ab4086a61aed5451134a19c933ee4db0d49ffec
Damiàn Grimaldi <grimaldidamian001@gmail.com>
2023-07-12 20:08:14 -0300

    Correccion de comprimir

--- comprimir.sh
+++ comprimir.sh
@@ -26,9 +26,8 @@ echo "La cantindad de personas que termian en a: $cont1" >> TerminanEnA.txt
 mkdir salida &> /dev/null
 
 tar -czvf imagenes.tar.gz *.jpg NombresImagenes.txt TerminanEnA.txt nombres.txt && echo Se creo el archivo imagenes.tar.gz correctamente && rm *.jpg *.txt
-mv imagenes.tar.gz salida/imagenes.tar.gz
-sleep 5s
 
 sha256sum imagenes.tar.gz | cut -d ' ' -f 1 > sumaVerificadora.txt && echo Se creo el archivo de la suma de verificacion
 mv sumaVerificadora.txt salida/sumaVerificadora.txt
+mv imagenes.tar.gz salida/imagenes.tar.gz
 sleep 5s
